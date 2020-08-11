0cefc208e13ffcc7e601e16f4ddac90f0af8fe53
Faizi512 <hfaizan53@gmail.com>
2020-08-11 13:22:40 +0500

    bug fix

--- app/controllers/api/retool_orders_controller.rb
+++ app/controllers/api/retool_orders_controller.rb
@@ -8,18 +8,21 @@ module Api
     def update
       if params.present?
         params[:Key1].each do |order|
-          RetoolOrder.find_or_create_by(order_id: order[:order_id]).update(
-            order_number: order[:order_number],
-            shipment_number: order[:shipment_number],
-            email: order[:email],
-            product_name: order[:product_name],
-            order_state: order[:order_state],
-            shipment_state: order[:shipment_state],
-            payment_state: order[:payment_state],
-            completed_at: order[:completed_at],
-            store_location_id: order[:store_location_id],
-            eta_date: order[:estimated_eta],
+          if order[:estimated_eta].present? || order[:contracted_date].present?
+            RetoolOrder.find_or_create_by(order_id: order[:order_id]).update(eta_date: order[:estimated_eta],
             contracted_date: order[:contracted_date])
+          else  
+            RetoolOrder.find_or_create_by(order_id: order[:order_id]).update(
+              order_number: order[:order_number],
+              shipment_number: order[:shipment_number],
+              email: order[:email],
+              product_name: order[:product_name],
+              order_state: order[:order_state],
+              shipment_state: order[:shipment_state],
+              payment_state: order[:payment_state],
+              completed_at: order[:completed_at],
+              store_location_id: order[:store_location_id])
+          end 
         end
       end
       render json: "success".to_json, status: :ok
