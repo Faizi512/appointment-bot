c228de5c4fe2e1c893b736a09bb327ac467c9327
Faizi512 <hfaizan53@gmail.com>
2020-08-11 12:18:10 +0500

    bug fix

--- app/controllers/api/retool_orders_controller.rb
+++ app/controllers/api/retool_orders_controller.rb
@@ -6,16 +6,14 @@ module Api
     end
 
     def update
-      puts "############"
-      puts "params #{params}"
       if params.present?
         params[:Key1].each do |order|
           RetoolOrder.find_or_create_by(order_id: order[:order_id]).update(
-            order_number: order[:Order_Number],
-            shipment_number: order[:Shipment_Number],
+            order_number: order[:order_number],
+            shipment_number: order[:shipment_number],
             email: order[:email],
-            product_name: order[:Product_Name],
-            order_state: order[:Order_State],
+            product_name: order[:product_name],
+            order_state: order[:order_state],
             shipment_state: order[:shipment_state],
             payment_state: order[:payment_state],
             completed_at: order[:completed_at],
