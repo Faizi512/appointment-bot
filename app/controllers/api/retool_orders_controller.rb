2a3a028515db996105e4f8724073d677d70f6283
Faizi512 <hfaizan53@gmail.com>
2020-08-11 13:12:04 +0500

    bug fix

--- app/controllers/api/retool_orders_controller.rb
+++ app/controllers/api/retool_orders_controller.rb
@@ -17,7 +17,7 @@ module Api
             shipment_state: order[:shipment_state],
             payment_state: order[:payment_state],
             completed_at: order[:completed_at],
-            store_location_id: order[:stock_location_id],
+            store_location_id: order[:store_location_id],
             eta_date: order[:estimated_eta],
             contracted_date: order[:contracted_date])
         end
