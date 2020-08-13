6b879796dbf301a249a0df91ed7075c8baf230a6
Faizi512 <hfaizan53@gmail.com>
2020-08-13 20:33:47 +0500

    delete all record which are shiped

--- app/controllers/api/retool_orders_controller.rb
+++ app/controllers/api/retool_orders_controller.rb
@@ -7,6 +7,7 @@ module Api
 
     def update
       if params.present?
+        process_oder_id = []
         params[:Key1].each do |order|
           data1 = {order_id: order[:order_id],
               order_number: order[:order_number],
@@ -22,12 +23,16 @@ module Api
               
           data2 = {eta_date: order[:estimated_eta],
               contracted_date: order[:contracted_date]}
+
           if order[:estimated_eta].present? || order[:contracted_date].present?    
             data1.merge!(data2)
           end
 
           RetoolOrder.find_or_create_by(item_id: order[:item_id],shipment_number: order[:shipment_number]).update(data1) 
+          dborder = RetoolOrder.find_by(item_id: order[:item_id],shipment_number: order[:shipment_number])
+          process_oder_id << dborder.id
         end
+        RetoolOrder.where.not(id: process_oder_id).delete_all
       end
       render json: "success".to_json, status: :ok
     end
