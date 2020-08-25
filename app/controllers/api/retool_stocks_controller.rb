7e7e094b105c81361485a3e11c810fa077e52aec
Faizi512 <hfaizan53@gmail.com>
2020-08-25 20:58:48 +0500

    update records by active job

--- app/controllers/api/retool_stocks_controller.rb
+++ app/controllers/api/retool_stocks_controller.rb
@@ -3,44 +3,9 @@ module Api
     protect_from_forgery except: :update
     def update
       if params.present?
-        process_stock_ids = []
-        count = 0
-        t14_products = LatestProduct.where(store_id: Store.find_by(name:"turn14").id)
-        ie_products = LatestProduct.where(store_id: Store.find_by(name:"performancebyie").id)
-        #ie_products means Integrated Engineering products coming from performancebyie
-        params[:Key1].each do |stock|
-          data1 = 
-            {
-              variant_id:           stock[:variant_id],
-              variant_name:         stock[:variant_name],
-              variant_sku:          stock[:variant_sku],
-              variant_mpn:          stock[:variant_mpn],
-              count_on_hand:        stock[:count_on_hand],
-              product_name:         stock[:product_name],
-              product_available_on: stock[:product_available_on],
-              stock_location_name:  stock[:stock_location_name],
-              brand_name:           stock[:brand_name]
-            }
-
-          if stock[:stock_location_name] == "Turn 14"
-            product = t14_products.find_by(sku: stock[:variant_sku])
-          elsif stock[:stock_location_name] == "Integrated Engineering"
-            product = ie_products.find_by(sku: stock[:variant_sku])
-          end
-          
-          if product.present?
-            data1[:count_on_hand] = product.inventory_quantity
-          end
-          
-          puts "Data_entry_count = #{count}"
-          count = count + 1
-          db_stock = RetoolStock.find_or_create_by(variant_id: stock[:variant_id],variant_sku: stock[:variant_sku])
-          db_stock.update(data1)
-          process_stock_ids << db_stock.id
-        end
-        RetoolStock.where.not(id: process_stock_ids).delete_all
+        UpdateRetoolStocksJob.perform_now(params)
+        render json: "success".to_json, status: :ok
       end
-      render json: "success".to_json, status: :ok
     end
 
   end
