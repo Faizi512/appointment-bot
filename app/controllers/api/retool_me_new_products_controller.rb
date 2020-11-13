33d6eb271a31dd1d67f2f57b5a2caf683512083e
Faizi512 <hfaizan53@gmail.com>
2020-11-13 18:04:11 +0500

    on on behalf of id

--- app/controllers/api/retool_me_new_products_controller.rb
+++ app/controllers/api/retool_me_new_products_controller.rb
@@ -5,7 +5,7 @@ module Api
     def update
       if params.present?
         params[:Key1].each do |rec|
-          product = MeNewProduct.find_or_create_by(fcpeuro_id: rec[:fcpeuro_id])
+          product = MeNewProduct.find_or_create_by(id: rec[:id])
           product.update(product_name: rec[:product_name], sku: rec[:sku], mpn: rec[:mpn], retail: rec[:retail], price: rec[:price], cost: rec[:cost]) if product.present?
         end
       end
@@ -13,7 +13,7 @@ module Api
     end
 
     def add_product
-      product = MeNewProduct.find_or_create_by(fcpeuro_id: params[:fcpeuro_id]) if params.present?
+      product = MeNewProduct.find_or_create_by(id: params[:id]) if params.present?
       product.update(status: 'Added') if product.present?
       render json: 'success'.to_json, status: :ok
     end
