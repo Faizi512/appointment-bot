535449fc8caaae32f5623f1bd457774b1b50b96a
Faizan Haider <faizan.haider@aldaimsolutions.com>
2021-10-25 15:18:06 +0500

    Mailer address update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,11 +4,11 @@ class UserMailer < ApplicationMailer
     #
     #   en.client_mailer.sendMail.subject
     #
-    default from: "faizan.haider@aldaimsolutions.com"
+    # default from: "faizan.haider@aldaimsolutions.com"
 
     def issue_in_script
-        # mail(to: ENV["Notify_Email"], subject: "Issue detected in script")
+        mail(to: ENV["MAILER_ADDRESS"], subject: "Issue detected in script")
         # mail(to: "noman.saleem@aldaimsolutions.com", subject: "Issue detected in script")
-        mail(to: "faizan.haider@aldaimsolutions.com", subject: "Issue detected in script")
+        # mail(to: "faizan.haider@aldaimsolutions.com", subject: "Issue detected in script")
     end
 end
