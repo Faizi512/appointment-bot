dcd80dd59191f4b0bfe377953fe995d8ae7aeea8
Faizan Haider <faizan.haider@aldaimsolutions.com>
2021-12-03 18:46:11 +0500

    API change for retool_orders

--- app/controllers/api/retool_orders_controller.rb
+++ app/controllers/api/retool_orders_controller.rb
@@ -9,42 +9,43 @@ module Api
       if params.present?
         process_oder_id = []
         params[:Key1].each do |order|
-          data1 = {order_id: order[:order_id],
-              order_number: order[:order_number],
-              shipment_number: order[:shipment_number],
-              email: order[:email],
-              product_name: order[:product_name],
-              order_state: order[:order_state],
-              shipment_state: order[:shipment_state],
-              payment_state: order[:payment_state],
-              completed_at: order[:completed_at],
-              store_location_id: order[:store_location_id],
-              stock_location_name: order[:stock_location_name]}
-              
-          data2 = {eta_date: order[:estimated_eta],
-              contracted_date: order[:contracted_date]}
 
-          if order[:estimated_eta].present? || order[:contracted_date].present?    
+          data1 = {order_id: params["Key1"][:order_id],
+              order_number: params["Key1"][:order_number],
+              shipment_number: params["Key1"][:shipment_number],
+              email: params["Key1"][:email],
+              product_name: params["Key1"][:product_name],
+              order_state: params["Key1"][:order_state],
+              shipment_state: params["Key1"][:shipment_state],
+              payment_state: params["Key1"][:payment_state],
+              completed_at: params["Key1"][:completed_at],
+              store_location_id: params["Key1"][:store_location_id],
+              stock_location_name: params["Key1"][:stock_location_name]}
+
+          data2 = {eta_date: params["Key1"][:estimated_eta],
+              contracted_date: params["Key1"][:contracted_date]}
+
+          if params["Key1"][:estimated_eta].present? || params["Key1"][:contracted_date].present?    
             data1.merge!(data2)
           end
 
+          data3 = {product_eta: params["Key1"][:brand_eta]}
 
-          data3 = {product_eta: order[:brand_eta]}
-
-          if order[:product_eta].present?
-            data3[:product_eta] = order[:product_eta]
+          if params["Key1"][:product_eta].present?
+            data3[:product_eta] = params["Key1"][:product_eta]
           end
 
-          if order[:stock_location_name] == "default"
+          if params["Key1"][:stock_location_name] == "default"
             data3[:product_eta] = "In Stock & Ships The Same Business Day"
           end
-          
+
           data1.merge!(data3)
 
-          dborder = RetoolOrder.find_or_create_by(item_id: order[:item_id],shipment_number: order[:shipment_number]) 
+          dborder = RetoolOrder.find_or_create_by(item_id: params["Key1"][:item_id],shipment_number: params["Key1"][:shipment_number]) 
           dborder.update(data1)
           process_oder_id << dborder.id
         end
+
         RetoolOrder.where.not(id: process_oder_id).delete_all
       end
       render json: "success".to_json, status: :ok
@@ -52,23 +53,20 @@ module Api
 
     def update_record
       if params.present?
-        params[:Key1].each do |order|
-          data1 = {order_id: order[:order_id],
-              order_number: order[:order_number],
-              shipment_number: order[:shipment_number],
-              email: order[:email],
-              product_name: order[:product_name],
-              order_state: order[:order_state],
-              shipment_state: order[:shipment_state],
-              payment_state: order[:payment_state],
-              completed_at: order[:completed_at],
-              store_location_id: order[:store_location_id],
-              stock_location_name: order[:stock_location_name],
-              eta_date: order[:estimated_eta],
-              contracted_date: order[:contracted_date]}
-
-          RetoolOrder.find_or_create_by(item_id: order[:item_id],shipment_number: order[:shipment_number]).update(data1)
-        end
+        data1 = {order_id: params["Key1"]["order_id"],
+            order_number: params["Key1"][:order_number],
+            shipment_number: params["Key1"][:shipment_number],
+            email: params["Key1"][:email],
+            product_name: params["Key1"][:product_name],
+            order_state: params["Key1"][:order_state],
+            shipment_state: params["Key1"][:shipment_state],
+            payment_state: params["Key1"][:payment_state],
+            completed_at: params["Key1"][:completed_at],
+            store_location_id: params["Key1"][:store_location_id],
+            stock_location_name: params["Key1"][:stock_location_name],
+            eta_date: params["Key1"][:estimated_eta],
+            contracted_date: params["Key1"][:contracted_date]}
+        RetoolOrder.find_or_create_by(item_id: params["Key1"][:item_id],shipment_number: params["Key1"][:shipment_number]).update(data1)
       end
       render json: "success".to_json, status: :ok
     end
