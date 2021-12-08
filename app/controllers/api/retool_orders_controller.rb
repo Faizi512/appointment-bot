b70c302da6d37bdbb5ce09e8fba2a73ca4b85144
Faizan Haider <faizan.haider@aldaimsolutions.com>
2021-12-08 14:13:29 +0500

    retool order api

--- app/controllers/api/retool_orders_controller.rb
+++ app/controllers/api/retool_orders_controller.rb
@@ -38,7 +38,7 @@ module Api
           dborder.update(data1)
           process_oder_id << dborder.id
         end
-        # RetoolOrder.where.not(id: process_oder_id).delete_all
+        RetoolOrder.where.not(id: process_oder_id).delete_all
       end
       render json: "success".to_json, status: :ok
     end
