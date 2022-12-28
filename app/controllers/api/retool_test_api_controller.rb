77e90eb4ea2f6d082c3f3d4c729b44034fd07e31
Faizi512 <hfaizan53@gmail.com>
2022-12-28 16:39:32 +0500

    Response message updated

--- app/controllers/api/retool_test_api_controller.rb
+++ app/controllers/api/retool_test_api_controller.rb
@@ -7,13 +7,14 @@ module Api
   class RetoolTestApiController < ApplicationController
     protect_from_forgery except: :test
     def test
+      byebug
       pdf = open(params["html"]["url"])
       session = GoogleDrive::Session.from_config('config/drive_config.json')
       response = session.upload_from_file(pdf, params["html"]["name"], convert: false, parents: ["1ZgzHBHzK2CZR54TFgly0ozM8aZdcpuXY"])
       if response.title.present?
-        render json: JSON.parse({code: 200, message: "File uploaded successfully"}), status: :ok
+        render json: {code: 200, message: "File uploaded successfully"}, status: :ok
       else
-        render json: JSON.parse({message: "File uploading filed"}), status: :error
+        render json: {message: "File uploading filed"}, status: :error
       end
     end
   end
